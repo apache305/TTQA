efd30c978ac0d1fe70fbe957e64a3b64f11468fc
apache305 <mengzide@gmail.com>
2015-06-15 10:46:35 +0200

    init

--- TemporalTQA/src/Others/GrosToT.java
+++ TemporalTQA/src/Others/GrosToT.java
@@ -16,12 +16,24 @@ public class GrosToT {
 	int G;//group number
 	int K;//topic number
 	int T;//time label?
+	int V;//this tag.
 	
 	
 	double [][] thetaGK;//group over topic distribution
+	int [][] ngk;
+	int []  sumngk;
+	
 	double [][] thetaUG;//user over group distribution
+	int [][] nug;
+	int [] sumnug;
+	
 	double [][] thetaKV;//topic over words distribution
+	int [][] nkv;
+	int [] sumnkv;
+	
 	double [][][] thetaGKT;//given group, topic over time distribution.
+	int [][][] ngkt;
+	int [][] sumngkt;
 	
 	
 	
@@ -42,6 +54,24 @@ public class GrosToT {
 	
 	public void initModel(Users users){
 		this.U = users.users.size();
+		this.T= users.timeCountMap.size();//number of time label
+		this.V= users.tagCountMap.size();//number of tag
+		
+		this.thetaGK = new double[this.G][this.K];
+		this.ngk=new int[this.G][this.K];
+		this.sumngk=new int[this.G];
+		
+		this.thetaUG = new double [this.U][this.G];
+		this.nug = new int [this.U][this.G];
+		this.sumnug= new int[this.U];
+		
+		this.thetaKV = new double [this.K][this.V];
+		this.nkv = new int[this.K][this.V];
+		this.sumnkv= new int[this.K];
+		
+		this.thetaGKT = new double [this.G][this.K][this.T];
+		this.ngkt = new int [this.G][this.K][this.T];
+		this.sumngkt = new int[this.G][this.K];
 		
 		
 	
