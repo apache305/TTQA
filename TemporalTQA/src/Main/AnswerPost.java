706a7a32c3630c7288c26273d061fe07eb797355
apache305 <mengzide@gmail.com>
2015-06-15 10:51:27 +0200

    make user and answer post public

---
+++ TemporalTQA/src/Main/AnswerPost.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class AnswerPost{
+	public int [] Qtags;
+	public int Atime;
+	public AnswerPost(int time, int[] tags) {
+		// TODO Auto-generated constructor stub
+		this.Atime=time;
+		this.Qtags=tags;
+	}
+	
+
+}
--- TemporalTQA/src/Main/Users.java
+++ TemporalTQA/src/Main/Users.java
@@ -93,63 +93,7 @@ public class Users {
 
 }
 
-class User {
-	String userId;
-	ArrayList<AnswerPost> answerPosts;
 
-	public User(String userId) {
-		// TODO Auto-generated constructor stub
-		this.userId=userId;
-		this.answerPosts = new ArrayList<AnswerPost>();
-	}
-	
-	public void addNewAnswerPost(ArrayList<String> eachAnswerInfo, Users users){
-		
-		String timeLabel = eachAnswerInfo.get(0).substring(0,7);
-		eachAnswerInfo.remove(0);
-		ArrayList<String> tagInfos = eachAnswerInfo;
-		//System.out.println(timeLabel);
-		int time= users.timeToIndexMap.get(timeLabel);
-		int oldCount=0;
-		oldCount=users.timeCountMap.get(timeLabel);
-		users.timeCountMap.put(timeLabel,oldCount+1);
-		int [] tags =  new int[tagInfos.size()];
-		for(int i=0;i<tagInfos.size();i++	){
-			String tag= tagInfos.get(i);
-			if( ! users.tagToIndexMap.containsKey(tag)){
-				int newIndex= users.tagToIndexMap.size();
-				users.indexToTagMap.add(tag);
-				users.tagToIndexMap.put(tag, newIndex);
-				users.tagCountMap.put(tag, 1);	
-				tags[i]=newIndex;
-			}else{
-				tags[i]=users.tagToIndexMap.get(tag);
-				oldCount= users.tagCountMap.get(tag);
-				users.tagCountMap.put(tag, oldCount+1);
-			}
-		}
-		AnswerPost ans= new AnswerPost(time,tags);
-		this.answerPosts.add(ans);
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-}
 
-class AnswerPost{
-	int [] Qtags;
-	int Atime;
-	public AnswerPost(int time, int[] tags) {
-		// TODO Auto-generated constructor stub
-		this.Atime=time;
-		this.Qtags=tags;
-	}
-	
 
-}
 
